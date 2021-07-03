a79132445840e388d1025e24aa5faed0a48683b3
crispg72 <chris.e.green@hotmail.com>
2021-07-03 16:02:38 +0100

    Do the acutal update

--- game-of-life.py
+++ game-of-life.py
@@ -5,6 +5,45 @@ import random
 GRID_SIZE = 50
 CELL_SIZE = 20
 
+
+def get_neighbours(grid, x, y):
+    num_neighbours = grid[(y - 1) % GRID_SIZE][(x - 1) % GRID_SIZE]
+    num_neighbours += grid[(y - 1) % GRID_SIZE][x]
+    num_neighbours += grid[(y - 1) % GRID_SIZE][(x + 1) % GRID_SIZE]
+    num_neighbours += grid[y][(x - 1) % GRID_SIZE]
+    num_neighbours += grid[y][(x + 1) % GRID_SIZE]
+    num_neighbours += grid[(y + 1) % GRID_SIZE][(x - 1) % GRID_SIZE]
+    num_neighbours += grid[(y + 1) % GRID_SIZE][x]
+    num_neighbours += grid[(y + 1) % GRID_SIZE][(x + 1) % GRID_SIZE]
+
+    return num_neighbours
+
+
+def update_grid(source_grid):
+    destination_grid = []
+    for y in range(GRID_SIZE):
+        destination_grid.append([])
+        for x in range(GRID_SIZE):
+            num_neighbours = get_neighbours(source_grid, x, y)
+
+            next_generation = 0
+
+            # Any live cell with two or three live neighbours survives.
+            if source_grid[y][x] == 1:
+                # Any live cell with two or three live neighbours survives.
+                if (num_neighbours > 1) and (num_neighbours < 4):
+                    next_generation = 1
+            else:
+                # Any dead cell with three live neighbours becomes a live cell.
+                if num_neighbours == 3:
+                    next_generation = 1
+
+            # All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+            destination_grid[y].append(next_generation)
+
+    return destination_grid
+
+
 def main():
     pygame.init()
     win = pygame.display.set_mode((GRID_SIZE * CELL_SIZE, GRID_SIZE * CELL_SIZE))
@@ -40,6 +79,8 @@ def main():
                     )
 
         pygame.display.flip()
+
+        grid = update_grid(grid)
         pygame.time.Clock().tick(60)
 
     pygame.quit()
